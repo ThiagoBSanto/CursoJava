4a0e7f1b3b88fff57b2fdc289c85d830be53a054
ThiagoBSanto <148291608+ThiagoBSanto@users.noreply.github.com>
2023-11-02 22:03:49 -0300

    challenge 149

---
+++ Chapter13/src/entities/Departament.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Departament {
+
+	private String name;
+	
+	public String Departament() {
+		return name;
+	}
+
+	public Departament(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+}
---
+++ Chapter13/src/entities/Worker.java
@@ -0,0 +1,89 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkLevel;
+
+public class Worker {
+	private String name;
+	private WorkLevel level;
+	private Double baseSalary;
+
+	private Departament departament;
+	private List<HourContract> contracts = new ArrayList<>();
+
+	public Worker() {
+	}
+
+	public Worker(String name, WorkLevel level, Double baseSalary, Departament departament) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.departament = departament;
+	}
+
+	// GETTERS AND SETTERS
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	// METODOS
+
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+
+
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+}
